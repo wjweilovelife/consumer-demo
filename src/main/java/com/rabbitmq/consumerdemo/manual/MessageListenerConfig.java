0d590999e3e91798a6fc3e3e41aa24c95269def4
王建伟 <wjw@choicesoft.com.cn>
2020-01-30 22:10:21 +0800

    fix:Initial commit

---
+++ src/main/java/com/rabbitmq/consumerdemo/manual/MessageListenerConfig.java
@@ -0,0 +1,44 @@
+package com.rabbitmq.consumerdemo.manual;
+
+import com.rabbitmq.consumerdemo.config.direct.DirectRabbitConfig;
+import com.rabbitmq.consumerdemo.config.direct.ManualDirectReceiver;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author : JCcccOØ
+ * @CreateTime : 2019/9/4ø
+ * @Description :
+ **/
+@Configuration
+public class MessageListenerConfig {
+
+    @Autowired
+    private CachingConnectionFactory connectionFactory;
+    @Autowired
+    private ManualDirectReceiver directReceiver;//Direct消息接收处理类
+    //    @Autowired
+//    FanoutReceiverA fanoutReceiverA;//Fanout消息接收处理类A
+    @Autowired
+    DirectRabbitConfig directRabbitConfig;
+
+    //    @Autowired
+//    FanoutRabbitConfig fanoutRabbitConfig;
+    @Bean
+    public SimpleMessageListenerContainer simpleMessageListenerContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory);
+        container.setConcurrentConsumers(1);
+        container.setMaxConcurrentConsumers(1);
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL); // RabbitMQ默认是自动确认，这里改为手动确认消息
+        container.setQueues(directRabbitConfig.TestDirectQueue());
+        container.setMessageListener(directReceiver);
+//        container.addQueues(fanoutRabbitConfig.queueA());
+//        container.setMessageListener(fanoutReceiverA);
+        return container;
+    }
+}
+
